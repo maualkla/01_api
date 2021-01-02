bf83f0834031f36efd55ceb30c3a250e4092fca0
maualkla <mauricio.alcala.4@gmail.com>
2021-01-02 13:19:13 -0600

    first_server

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("hello world");
+/*
+ *	Primary file for the API.
+ *
+ *
+ */ 
+
+ // Dependencies
+ const http = require("http");
+
+
+ // The server should respond to all request with a string :)
+var server = http.createServer(function(req, res){
+	res.end("Hello world!\n");
+});
+
+
+ // Start the server, and have it listen to port 3000.
+server.listen(3000, function(){
+	console.log("the server is listening in port 3000 now.");
+});
