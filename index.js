6399fb840d1c7fd8969ecd20516331212a2efa06
maualkla <mauricio.alcala.4@gmail.com>
2021-01-02 14:00:32 -0600

    functional_path

--- index.js
+++ index.js
@@ -4,17 +4,30 @@
  *
  */ 
 
- // Dependencies
- const http = require("http");
+// Dependencies
+const http = require("http");
+const url = require("url");
 
-
- // The server should respond to all request with a string :)
+// The server should respond to all request with a string :)
 var server = http.createServer(function(req, res){
+
+
+	// Get the URL and parse it.
+	var parsedUrl = url.parse(req.url, true);
+
+	// Get the URL path.
+	var path = parsedUrl.pathname;
+	var trimedPath = path.replace(/^\/+|\/+$/g,'');
+
+	// Send the response.
 	res.end("Hello world!\n");
+
+	// Log the request path.
+	console.log(" Request received on path: " + trimedPath);
 });
 
 
- // Start the server, and have it listen to port 3000.
+// Start the server, and have it listen to port 3000.
 server.listen(3000, function(){
 	console.log("the server is listening in port 3000 now.");
 });
