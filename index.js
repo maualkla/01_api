2e90cd641eaa6bc8d88288fc5da3bb5fbfed3a14
maualkla <mauricio.alcala.4@gmail.com>
2021-03-09 21:14:22 -0600

    adding_helpers_to_index

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const StringDecoder = require("string_decoder").StringDecoder;
 const config = require('./config');
 const fs = require('fs');
 const handlers = require("./lib/handlers");
+const helpers = require('./lib/helpers');
 
 /*
 var _data = require('./lib/data');
@@ -85,7 +86,7 @@ var unifiedServer = function(req, res){
 			'queryStringObject' : queryStringObject,
 			'method' : method, 
 			'headers' : headers, 
-			'payload' : buffer
+			'payload' : helpers.parseJsonToObject(buffer)
 		}
 
 		// Route the request to the handler specified in the router
