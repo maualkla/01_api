be85d2a5d30b6c1bd97600452126be390eafb793
maualkla <mauricio.alcala.4@gmail.com>
2021-10-24 19:08:37 -0500

    dandole again

---
+++ config.js
@@ -0,0 +1,34 @@
+/*
+ *	Create and export configuration variables.
+ *
+ */ 
+
+ // General container for all the enviroments.
+ var enviroments = {};
+
+ // Staging enviroment
+ enviroments.staging = {
+ 	'httpPort': 3000,
+ 	'httpsPort': 3001,
+ 	'envName': 'staging'
+ };
+
+ // Production Enviroment
+ enviroments.production = {
+ 	'httpPort': 5000, 
+ 	'httpsPort': 5001,
+ 	'envName': 'production'
+ };
+
+
+// Determine the enviroment to be returned
+var currentEnviroment = typeof(process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLowerCase() : '';
+
+
+// Check th current enviroment in one of the variables avobe. if not default to stagging
+var enviromentToExport = typeof(enviroments[currentEnviroment]) == 'object' ? enviroments[currentEnviroment] : enviroments.staging;
+
+// export the module
+module.exports = enviromentToExport;
+
+//openssl req rsa:2048 -new -nodes -x509 -days 3650 -keyout key.pem -out cert.pem
---
+++ lib (1)/helpers.js
@@ -0,0 +1,70 @@
+/*
+ *	Helpers for various tasks.
+ *
+ *
+ */ 
+
+// Dependencies
+const crypto = require('crypto');
+const config = require('./config');
+
+// Container for all the helpers.
+var helpers = {};
+
+
+// Create a SHA 256 hash 
+helpers.hash = function(str){
+	if(typeof(str) == 'string' && str.length > 0){
+		var hash = crypto.createHmac('sha256', config.hashingSecret).update(str).digest('hex');
+		return hash;
+	}else{
+		return false;
+	}
+}
+
+// Create paseJsonToObject helper
+// Parse a Json string to an object in all cases, without trowing.
+helpers.parseJsonToObject = function(str){
+	try{
+		var obj = JSON.parse(str);
+		return obj;
+	}catch(e){
+		return {};
+	}
+};
+
+
+// Helper to create a random string of the lenght of the input number.
+helpers.createRandomString = function(strLenght){
+	strLenght = typeof(strLenght) == 'number' && strLenght > 0 ? strLenght : false;
+	if(strLenght){
+		// Define all posible characters we will use to create the random string.
+		var possibleCharacters = 'abcdefghiklmnopqrstuvwxyz0123456789';
+
+		// Start the final string.
+		var str = '';
+		for(i = 1; i <= strLenght; i++){
+			// get a random character from all the possible characteres
+			var randomCharacter = possibleCharacters.charAt(Math.floor(Math.random() * possibleCharacters.length));
+			// Append this character to the final string.
+			str += randomCharacter;
+		}
+
+		// Return the final string.
+		return str;
+	}else{
+		return(false);
+	}
+};
+	
+
+
+
+
+
+
+
+
+
+// Exports the module.
+module.exports = helpers; // https://www.pirple.com/courses/take/the-nodejs-master-class/lessons/3809865-service-2-users 18:29
